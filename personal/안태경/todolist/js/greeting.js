4f5b94710b4880caa60ce6019193bd749dd1043f
TaekyungAn <atk0426@naver.com>
2023-06-20 17:36:36 +0900

    Comment: 주석 수정

--- personal/안태경/todolist/js/greeting.js
+++ personal/안태경/todolist/js/greeting.js
@@ -21,7 +21,7 @@ function paintGreetings(username) {
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 if (savedUsername === null) {
   // 저장된 이름 없음 => 새 이름 받기
-  loginForm.classList.remove(HIDDEN_CLASSNAME); //로그인 폼 없애기
+  loginForm.classList.remove(HIDDEN_CLASSNAME); //로그인 폼 나타내기
   loginForm.addEventListener("submit", getUsername); // 폼 제출하기 (이름받는과정시작)
 } else {
   // 저장된 이름 있음 =>저장된 이름 가져오기
