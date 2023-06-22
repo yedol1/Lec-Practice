99f407a0d8928fe493576609ca52c2fa00efcefd
TaekyungAn <atk0426@naver.com>
2023-06-22 16:24:56 +0900

    Feat: 시각표시 형태 수정

--- personal/안태경/todolist/js/time.js
+++ personal/안태경/todolist/js/time.js
@@ -1,11 +1,37 @@
 const date = document.querySelector("h3#date");
 const clock = document.querySelector("h2#clock");
+const hour = clock.querySelector("#hour");
+const minute = clock.querySelector("#minute");
+const second = clock.querySelector("#second");
+const ampm = document.querySelector("#ampm");
 
 function getDate() {
   date.innerText = new Date().toLocaleDateString();
 }
 function getClock() {
-  clock.innerText = new Date().toLocaleTimeString();
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  let addZero;
+  if (hours === 12) {
+    addZero = "12";
+    ampm.innerText = "오후";
+  } else if (hours > 12) {
+    addZero = String(hours - 12).padStart(2, "0");
+    ampm.innerText = "오후";
+  } else if (hours === 0) {
+    addZero = "12";
+    ampm.innerText = "오전";
+  } else {
+    addZero = String(hours).padStart(2, "0");
+    ampm.innerText = "오전";
+  }
+
+  hour.innerText = `${addZero}`;
+  minute.innerText = `${minutes}`;
+  second.innerText = `${seconds}`;
 }
 
 getDate();
