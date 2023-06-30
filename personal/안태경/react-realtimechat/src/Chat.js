25a5e24397b58b72d6341080cb20c0e96102b949
TaekyungAn <atk0426@naver.com>
2023-06-30 16:45:10 +0900

    Fix: typo

--- personal/안태경/react-realtimechat/src/Chat.js
+++ personal/안태경/react-realtimechat/src/Chat.js
@@ -17,12 +17,14 @@ function Chat() {
   const handleMessage = (message) => {
     setMessages((prev) => [...prev, message]);
   };
-
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+  };
   const handleSubmit = () => {
     if (inputValue.trim() !== "") {
       const currentTime = new Date().toLocaleDateString();
       socket.emit("message", {
-        username,
+        name: username,
         content: inputValue,
         time: currentTime,
       });
@@ -32,28 +34,31 @@ function Chat() {
   return (
     <div>
       <h1>실시간 채팅</h1>
-
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder="사용자 이름을 입력하세요."
-      />
-      <h2>채팅</h2>
-      <div>
-        {messages.map((message, index) => (
-          <p key={index}>
-            {messages.name} : {messages.content} - {messages.time}
-          </p>
-        ))}
-      </div>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-      />
-      <button onClick={handleSubmit}>전송</button>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="사용자 이름을 입력하세요."
+        />
+        <h2>채팅</h2>
+        <div>
+          {messages.map((message, index) => (
+            <p key={index}>
+              {message.name} : {message.content} - {message.time}
+            </p>
+          ))}
+        </div>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button onClick={handleSubmit}>전송</button>
+      </form>
     </div>
   );
 }
 export default Chat;
+
+// node server.js =>서버작동
